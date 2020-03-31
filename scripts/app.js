3709a6dd1eeec8650fcce5dfa3aa1e04bd262e86
Karthik M S <ms18karthik@gmail.com>
2020-04-01 02:43:24 +0530

    Adding basic layout for the app

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,25 @@ $(".main-banner").slick({
   arrows: false,
   autoplay: true
 });
+
+$("#mr_kats_show_slider").slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 620,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 345,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
