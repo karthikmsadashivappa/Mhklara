335ff4d131c200b9a160d91be7a74080b0f82994
Karthik M S <ms18karthik@gmail.com>
2020-04-05 02:51:14 +0530

    finishing images and links

--- scripts/app.js
+++ scripts/app.js
@@ -28,3 +28,52 @@ $("#mr_kats_show_slider").slick({
     }
   ]
 });
+
+$("#top_stories_slider").slick({
+  dots: false,
+  infinite: true,
+  slidesToScroll: 1,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 9999,
+      settings: "unslick"
+    },
+    {
+      breakpoint: 620,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 345,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+$(".pop").on("click", function(e) {
+  var wrapper = $("#video-modal");
+  var href = $(this).attr("href");
+  var customFrame =
+    "<iframe src=" +
+    href +
+    ' width="100%" height="100%" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" class="youtube-video"></iframe>';
+  $(wrapper)
+    .append(customFrame)
+    .show();
+  e.preventDefault();
+});
+
+var closeButton = document.querySelector(".close-button");
+closeButton.addEventListener("click", function() {
+  document.querySelector("#video-modal").style.display = "none";
+  document.querySelector(".ytp-cued-thumbnail-overlay").style.display = "none";
+});
+
+$(".close-button").click(function() {
+  document.querySelector(".youtube-video").src = "";
+  window.location.reload();
+});
